4ce1789dc87a06d0fdb7083343540f728044ad59
Harol <darksxd@live.com>
2021-04-01 23:07:08 -0500

    chore() test

--- src/components/MyComponent/MyComponent.test.tsx
+++ src/components/MyComponent/MyComponent.test.tsx
@@ -3,7 +3,9 @@ import { render } from '@testing-library/react'
 import MyComponent from './MyComponent'
 
 test('Renders', async () => {
-  const { getByRole } = render(<MyComponent name="John" />)
+  const { getByRole } = render(
+    <MyComponent name="John" number="12" text="hola" />
+  )
 
   expect(getByRole('heading')).toHaveTextContent('My First Component: John')
 })
