b39a4ab2b8a8258f5379ab4d600a7b7e26604cb0
Harol <darksxd@live.com>
2021-04-04 16:41:05 -0500

    fix

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,7 +1,7 @@
 //Don't modify this file directly because is automaticaly generated - check create-main-js.js file in scripts folder
-import Button from "./Button/Button";
-import Icon from "./Icon/Icon";
-import Loader from "./Loader/Loader";
-import MyComponent from "./MyComponent/MyComponent";
+import Button from './Button/Button'
+import Icon from './Icon/Icon'
+import Loader from './Loader/Loader'
+import MyComponent from './MyComponent/MyComponent'
 
-export { Button, Icon, Loader, MyComponent };
+export { Button, Icon, Loader, MyComponent }
