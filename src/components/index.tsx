806851dce9e5395a2380028b9b127255fa637121
Harol <darksxd@live.com>
2021-04-04 15:44:59 -0500

    chore()

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,6 +1,6 @@
 //Don't modify this file directly because is automaticaly generated - check create-main-js.js file in scripts folder
 import Button from './Button/Button'
-import Icon from './Icon/Icon'
+import Icon from '/Icon/Icon'
 import Loader from './Loader/Loader'
 import MyComponent from './MyComponent/MyComponent'
 
